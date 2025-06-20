35043ce561cce6db2d20efc6befc46675b3801fb
ShunKUN1182 <2250378@ecc.ac.jp>
2025-06-20 15:23:49 +0900

    バグ

--- js/baseball_01.js
+++ js/baseball_01.js
@@ -3,36 +3,96 @@ const highBtn = document.querySelector("#highBtn");
 const middleBtn = document.querySelector("#middleBtn");
 const lowBtn = document.querySelector("#lowBtn");
 
-let opponentRandom = Math.floor(Math.random()*3);
-let randomA = Math.floor(Math.random()*11);
-let opponentCourse = [];
 
 
 
 // 0を高め、1を真ん中、2を低めとする
 
-if (opponentRandom === 0) {
-    opponentCourse = "高め"
-}else if (opponentRandom === 1) {
-    opponentCourse = "真ん中"
-}else if (opponentRandom === 2) {
-    opponentCourse = "低め"
-}else{
-    alert("error");
-}
 
 highBtn.addEventListener("click",()=>{
-    if (opponentCourse === "高め") {
-        console.log("hit");
+
+    let opponentRandom = Math.floor(Math.random()*3);
+    let randomA = Math.floor(Math.random()*11);
+    let opponentCourse = [];
+
+    if (opponentRandom === 0) {
+        opponentCourse = "高め"
+    }else if (opponentRandom === 1) {
+        opponentCourse = "真ん中"
+    }else if (opponentRandom === 2) {
+        opponentCourse = "低め"
     }else{
+        alert("error");
+    }
+    
+    if (opponentCourse === "高め") {
         if (randomA <= 3) {
-            console.log("ストライク");
+            console.log("ボール");
         }else if (randomA <= 6) {
-            console.log("ファール");
-        }else if (randomA <=9) {
-        console.log("ボール");
+            console.log("ボール");
+        }else if (randomA <=7) {
+            console.log("ホームラン");
+        }else if (randomA <= 9) {
+            console.log("ヒット");
         }else if (randomA <=10) {
             console.log("アウト");
         }
-    }
+        }else if (randomA <= 3) {
+            console.log("ストライク");
+        }else if (randomA <= 6) {
+            console.log("アウト");
+        }else if (randomA <=9) {
+            console.log("ボール");
+        }else if (randomA <=10) {
+            console.log("アウト");
+        }
+        else if (opponentCourse === "真ん中") {
+            if (randomA <= 3) {
+                console.log("ボール");
+            }else if (randomA <= 6) {
+                console.log("ボール");
+            }else if (randomA <=7) {
+                console.log("ホームラン");
+            }else if (randomA <= 9) {
+                console.log("ヒット");
+            }else if (randomA <=10) {
+                console.log("アウト");
+            }
+        }else{
+            if (randomA <= 3) {
+                console.log("ストライク");
+            }else if (randomA <= 6) {
+                console.log("アウト");
+            }else if (randomA <=9) {
+                console.log("ボール");
+            }else if (randomA <=10) {
+                console.log("アウト");
+            }
+        }
+        
+        
+        if (opponentCourse === "低め") {
+            if (randomA <= 3) {
+                console.log("ボール");
+            }else if (randomA <= 6) {
+                console.log("ボール");
+            }else if (randomA <=7) {
+                console.log("ホームラン");
+            }else if (randomA <= 9) {
+                console.log("ヒット");
+            }else if (randomA <=10) {
+                console.log("アウト");
+            }
+        }else{
+            if (randomA <= 3) {
+                console.log("ストライク");
+            }else if (randomA <= 6) {
+                console.log("アウト");
+            }else if (randomA <=9) {
+                console.log("ボール");
+            }else if (randomA <=10) {
+                console.log("アウト");
+            }
+        }
 })
+    
