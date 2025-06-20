23c9921fda9e961d22c61de24791de85ac76b6d6
ShunKUN1182 <2250378@ecc.ac.jp>
2025-06-20 15:41:47 +0900

    すべてのボタンを対応

--- js/baseball_01.js
+++ js/baseball_01.js
@@ -49,3 +49,88 @@ highBtn.addEventListener("click",()=>{
         }
     }
 })
+
+
+middleBtn.addEventListener("click",()=>{
+
+    let opponentRandom = Math.floor(Math.random()*3);
+    let randomA = Math.floor(Math.random()*11);
+    let opponentCourse = [];
+
+    if (opponentRandom === 0) {
+        opponentCourse = "高め"
+    }else if (opponentRandom === 1) {
+        opponentCourse = "真ん中"
+    }else if (opponentRandom === 2) {
+        opponentCourse = "低め"
+    }else{
+        alert("error");
+    }
+    
+    if (opponentCourse === "真ん中") {
+        if (randomA <= 3) {
+            console.log("ボール");
+        }else if (randomA <= 6) {
+            console.log("ボール");
+        }else if (randomA <=7) {
+            console.log("ホームラン");
+        }else if (randomA <= 9) {
+            console.log("ヒット");
+        }else if (randomA <=10) {
+            console.log("アウト");
+        }
+        }else{
+            if(randomA <= 3) {
+            console.log("ストライク");
+        }else if (randomA <= 6) {
+            console.log("アウト");
+        }else if (randomA <=9) {
+            console.log("ストライク");
+        }else{
+            console.log("ボール");
+        }
+    }
+})
+
+
+
+lowBtn.addEventListener("click",()=>{
+
+    let opponentRandom = Math.floor(Math.random()*3);
+    let randomA = Math.floor(Math.random()*11);
+    let opponentCourse = [];
+
+    if (opponentRandom === 0) {
+        opponentCourse = "高め"
+    }else if (opponentRandom === 1) {
+        opponentCourse = "真ん中"
+    }else if (opponentRandom === 2) {
+        opponentCourse = "低め"
+    }else{
+        alert("error");
+    }
+    
+    if (opponentCourse === "低め") {
+        if (randomA <= 3) {
+            console.log("ボール");
+        }else if (randomA <= 6) {
+            console.log("ボール");
+        }else if (randomA <=7) {
+            console.log("ホームラン");
+        }else if (randomA <= 9) {
+            console.log("ヒット");
+        }else if (randomA <=10) {
+            console.log("アウト");
+        }
+        }else{
+            if(randomA <= 3) {
+            console.log("ストライク");
+        }else if (randomA <= 6) {
+            console.log("アウト");
+        }else if (randomA <=9) {
+            console.log("ストライク");
+        }else{
+            console.log("ボール");
+        }
+    }
+})
